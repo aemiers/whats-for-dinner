3c6159fb18847057a9297f888d732cb8fdcdbdd2
Anneke Miers <annekemiers.am@gmail.com>
2020-12-18 23:56:17 -0700

    Add querySelectors, event listeners, and bare bones for generateRecipe function

---
+++ main.js
@@ -0,0 +1,25 @@
+// ~~~~~~~~~~~~~~~~~ query selectors  ~~~~~~~~~~~~~~~~~~~~~~~~
+var addARecipeButton = document.querySelector("#add-recipe");
+var letsCookButton = document.querySelector("#lets-cook");
+var clearButton = document.querySelector("#clear-button");
+
+// ~~~~~~~~~~~~~~~~~ event listeners ~~~~~~~~~~~~~~~~~~~~~~~~
+addARecipeButton.addEventListener('click', addARecipe);
+letsCookButton.addEventListener('click', generateRecipe);
+clearButton.addEventListener('click', clearRecipe);
+
+// ~~~~~~~~~~~~~~~~~ global variables ~~~~~~~~~~~~~~~~~~~~~~~~
+var savedRecipes = [];
+var currentRecipe;
+
+function generateRecipe() {
+
+}
+
+function clearRecipe() {
+
+}
+
+function addARecipe() {
+
+}
