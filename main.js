24f71c8032825ace6182bc164ed8e2aa604d3d0c
Anneke Miers <annekemiers.am@gmail.com>
2020-12-19 00:00:45 -0700

    Add toggleHidden function

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var letsCookButton = document.querySelector("#lets-cook");
 var clearButton = document.querySelector("#clear-button");
 
 // ~~~~~~~~~~~~~~~~~ event listeners ~~~~~~~~~~~~~~~~~~~~~~~~
-addARecipeButton.addEventListener('click', addARecipe);
-letsCookButton.addEventListener('click', generateRecipe);
-clearButton.addEventListener('click', clearRecipe);
+addARecipeButton.addEventListener("click", addARecipe);
+letsCookButton.addEventListener("click", generateRecipe);
+clearButton.addEventListener("click", clearRecipe);
 
 // ~~~~~~~~~~~~~~~~~ global variables ~~~~~~~~~~~~~~~~~~~~~~~~
 var savedRecipes = [];
@@ -14,6 +14,10 @@ var currentRecipe;
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+function toggleHidden(element) {
+  element.classList.toggle("hidden");
 }
 
 function generateRecipe() {
